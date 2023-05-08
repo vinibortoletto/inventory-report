a804f753fae409e476de5123186123c6a9a72620
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-08 18:43:33 -0300

    fix: duplicity when generation complete report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -2,28 +2,27 @@ from inventory_report.reports.simple_report import SimpleReport
 
 
 class CompleteReport(SimpleReport):
-    def get_product_quantity_by_company(product_list, company_name):
-        return len(
-            [
-                product
-                for product in product_list
-                if product["nome_da_empresa"] == company_name
-            ]
-        )
+    def get_company_product_qty_dict(product_list):
+        company_dict = dict()
+
+        for product in product_list:
+            company_name = product["nome_da_empresa"]
+
+            if company_name not in company_dict:
+                company_dict[company_name] = 1
+            else:
+                company_dict[company_name] += 1
+
+        return company_dict
 
     @classmethod
     def generate(self, product_list):
         simple_report = SimpleReport.generate(product_list)
         complete_report = simple_report
         complete_report += "\nProdutos estocados por empresa:\n"
+        company_dict = self.get_company_product_qty_dict(product_list)
 
-        for product in product_list:
-            company_name = product["nome_da_empresa"]
-
-            quantity = self.get_product_quantity_by_company(
-                product_list, company_name
-            )
-
-            complete_report += f"- {company_name}: {quantity}\n"
+        for company, qty in company_dict.items():
+            complete_report += f"- {company}: {qty}\n"
 
         return complete_report
