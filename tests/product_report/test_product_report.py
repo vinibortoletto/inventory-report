a5062e8315ab6d42c65b78cce40fdbcd39ca16dd
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-08 20:08:18 -0300

    test: "relatorio_produto"

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,34 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+from faker import Faker
+
+fake = Faker("pt-BR")
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    id = fake.random_number(digits=1)
+    nome_do_produto = fake.word()
+    nome_da_empresa = fake.company()
+    data = fake.date()
+    numero_de_serie = fake.random_number(digits=8)
+    instrucoes_de_armazenamento = fake.sentence()
+
+    product = Product(
+        id,
+        nome_do_produto,
+        nome_da_empresa,
+        data,
+        data,
+        numero_de_serie,
+        instrucoes_de_armazenamento,
+    )
+
+    actual = str(product)
+    expected = (
+        f"O produto {nome_do_produto}"
+        f" fabricado em {data}"
+        f" por {nome_da_empresa} com validade"
+        f" até {data}"
+        f" precisa ser armazenado {instrucoes_de_armazenamento}."
+    )
+
+    assert actual == expected
