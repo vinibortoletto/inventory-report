9071724f8344ef1d0ddb0bb23f7d622b4e9a6f08
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-08 18:52:28 -0300

    feat: import data from json file

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,14 +1,28 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
-    @classmethod
-    def import_data(self, path, report_type):
+    def get_content_from_csv(path):
         with open(path, encoding="utf-8") as file:
             file_content = csv.DictReader(file)
-            product_list = [row for row in file_content]
+            return [row for row in file_content]
+
+    def get_content_from_json(path):
+        with open(path) as file:
+            return json.load(file)
+
+    @classmethod
+    def import_data(self, path, report_type):
+        file_type = path.split(".")[1]
+        product_list = []
+
+        if file_type == "csv":
+            product_list = self.get_content_from_csv(path)
+        elif file_type == "json":
+            product_list = self.get_content_from_json(path)
 
         report = ""
 
